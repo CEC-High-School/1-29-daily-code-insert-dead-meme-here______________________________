7338258a3f833583b7d0c8607ab29284638a65aa
gorillageek27 <35504088+gorillageek27@users.noreply.github.com>
2018-01-29 13:04:13 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
 
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+	double a;
+	cout << "give me the radius" << endl;
+
+	cin >> a;
+
+	cout << "area: " << 3.14 * a *a << endl;
+
+	cout << "circumference" << 2 * 3.14* a << endl;;
+
+
+
+}
+	
